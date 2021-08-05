ad2877cd01c43300c854ee8a14d833674210434a
Yuran Fernando <yuran.fernando@globaltote.com>
2021-08-05 10:31:58 +0530

    Added the start dates.

--- api/app/Http/Controllers/WeeklyRetentionChartController.php
+++ api/app/Http/Controllers/WeeklyRetentionChartController.php
@@ -3,16 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Services\Charts\WeeklyRetentionService;
+use App\Services\Http\ApiResponse;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class WeeklyRetentionChartController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __invoke(WeeklyRetentionService $weeklyRetentionService)
+    protected $response;
+    protected $request;
+    protected $service;
+
+    public function __construct(
+        WeeklyRetentionService $service,
+        ApiResponse $response,
+        Request $request
+    ) {
+        $this->response = $response;
+        $this->request = $request;
+        $this->service = $service;
+    }
+
+    public function __invoke(): JsonResponse
     {
-        return $weeklyRetentionService->getChartData(['start_date' => '2016-07-19']);
+        $startDate = $this->request->get('start_date', '2016-07-19');
+
+        $data =  $this->service->getChartData(['start_date' => $startDate]);
+
+        return $this->response->success($data);
     }
 }
